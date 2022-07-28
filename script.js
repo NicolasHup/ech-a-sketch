6ff793271d9d5588f7f837defcd139da10451a69
Nicolas <malulekanicolas@gmail.com>
2022-07-28 15:11:09 +0200

    16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,11 @@
+let container = document.querySelector(".container");
+container.style.border = "solid 1px rgb(167, 162, 162)";
+container.style.gridTemplateColumns = "repeat(16 , 1fr)";
+container.style.gridTemplateRows = "repeat(16 , 1fr)";
+
+// this will populate the main div with 16x16 divs
+for (let i = 0; i < 256; i++){
+    let square = document.createElement("div");
+    square.style.border = "solid 1px rgb(167, 162, 162)";
+    container.insertAdjacentElement("beforeend",square);
+}

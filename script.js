e602bcafcf255a4ca7de9a3658a45e26a1cda876
NicolasHup <malulekanicolas@gmail.com>
2022-08-11 11:23:41 +0200

    Added hover effect

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ container.style.gridTemplateRows = "repeat(16 , 1fr)";
 for (let i = 0; i < 256; i++){
     let square = document.createElement("div");
     square.style.border = "solid 1px rgb(167, 162, 162)";
+
+    //changes the color of the box when hover over the grid
+    square.addEventListener('mouseenter', () =>{
+        square.style.backgroundColor = 'black';
+    })
     container.insertAdjacentElement("beforeend",square);
 }
